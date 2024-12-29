5281fa000ccbd5f8d263fa461507ffc216b1f258
faisal shahzad <faisal712000@gmail.com>
2024-12-29 14:50:33 +0500

    step 8 done

--- main.go
+++ main.go
@@ -7,9 +7,30 @@ import (
 	"os/exec"
 	"strings"
 	"os/signal"
+	"io/ioutil"
+	"os/user"
 )
 
+var history []string
+
+func loadHistory() {
+	usr, _ := user.Current()
+	historyFile := usr.HomeDir + "/.ccsh_history"
+	if data, err := ioutil.ReadFile(historyFile); err == nil {
+		history = strings.Split(string(data), "\n")
+	}
+}
+
+func saveHistory() {
+	usr, _ := user.Current()
+	historyFile := usr.HomeDir + "/.ccsh_history"
+	ioutil.WriteFile(historyFile, []byte(strings.Join(history, "\n")), 0644)
+}
+
 func main() {
+
+	loadHistory()
+	defer saveHistory()
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
@@ -55,6 +76,8 @@ func main() {
 		if err := cmd.Run(); err != nil {
 			fmt.Println("No such file or directory:", err)
 		}
+
+		history = append(history, command)
 	}
 }
 
