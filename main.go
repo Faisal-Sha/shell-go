ae03aec9df039fc2c3f88222c8141f96dc8002ce
faisal shahzad <faisal712000@gmail.com>
2024-12-29 14:43:36 +0500

    step 5 done

--- main.go
+++ main.go
@@ -25,6 +25,22 @@ func main() {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 
+		if args[0] == "cd" {
+			if len(args) < 2 {
+				fmt.Println("Usage: cd <directory>")
+			} else if err := os.Chdir(args[1]); err != nil {
+				fmt.Println("Error:", err)
+			}
+			continue
+		} else if args[0] == "pwd" {
+			if dir, err := os.Getwd(); err == nil {
+				fmt.Println(dir)
+			} else {
+				fmt.Println("Error:", err)
+			}
+			continue
+		}
+
 		if err := cmd.Run(); err != nil {
 			fmt.Println("No such file or directory:", err)
 		}
